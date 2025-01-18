cad1a3fd6f4f35e8329c3a7d0314779b6f194203
Nicky <selfnicky@gmail.com>
2025-01-18 19:55:25 +0100

    Updated 2-EC2-instance-with-Terraform-

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance1" {
-  ami           = "ami-0454e52560c7f5c55"  # AMI for the first instance
+  ami           = "ami-0df8c184d5f6ae949"  # AMI for the first instance
   instance_type = "t2.micro"               # Instance type
   key_name      = "mykey"                  # Replace with your key pair name (without .pem)
 
   tags = {
-    Name = "Instance1"
+    Name = "NewInstance1"
   }
 }
 
 resource "aws_instance" "instance2" {
-  ami           = "ami-0e1bed4f06a3b463d"  # AMI for the second instance
+  ami           = "ami-043a5a82b6cf98947"  # AMI for the second instance
   instance_type = "t2.micro"               # Instance type
   key_name      = "mykey"                  # Replace with your key pair name (without .pem)
 
   tags = {
-    Name = "Instance2"
+    Name = "NewInstance2"
   }
 }
